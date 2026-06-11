66c65c437619b635a8d938489ca3c19497e11e6c
William Harwood <william.harwood@nominal.io>
2026-06-11 14:53:40 -0700

    feat(psu): update bk 914X with OVP, OCP, and remote sense

--- instro/psu/drivers/__init__.py
+++ instro/psu/drivers/__init__.py
@@ -1,8 +1,8 @@
 """PSU drivers package."""
 
 from instro.psu import PSUDriverBase
+from instro.psu.drivers.bk_914x import BK914X
 from instro.psu.drivers.bk_9115 import BK9115
-from instro.psu.drivers.bk_9140 import BK9140
 from instro.psu.drivers.keysight_e36100 import KeysightE36100
 from instro.psu.drivers.rigol_dp800 import RigolDP800
 from instro.psu.drivers.siglent_spd3303 import SiglentSPD3303
@@ -12,7 +12,7 @@ from instro.psu.drivers.tdk_lambda_genesys import TDKLambdaGenesys
 __all__ = [
     "PSUDriverBase",
     "BK9115",
-    "BK9140",
+    "BK914X",
     "KeysightE36100",
     "RigolDP800",
     "SiglentSPD3303",
