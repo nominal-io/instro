0863ca700f2ced53132a72b1de162269fddfe3f6
Carter Canedy <cartercanedy42@gmail.com>
2026-07-16 12:45:39 -0700

    fix sdist smoke test script

--- tests/ethernetip/check_ethernetip_sdist.py
+++ tests/ethernetip/check_ethernetip_sdist.py
@@ -98,7 +98,7 @@ def _local_path_dependencies() -> dict[str, Path]:
 def _expected_paths() -> set[ExpectedPath]:
     expected = set()
     expected.add(ExpectedPath("wrapper manifest", "Cargo.toml", _archive_path(WRAPPER_ARCHIVE_DIR, "Cargo.toml")))
-    expected.add(ExpectedPath("workspace manifest", "Cargo.toml", _archive_path(WRAPPER_ARCHIVE_DIR, "Cargo.toml")))
+    expected.add(ExpectedPath("workspace manifest", "Cargo.toml", "Cargo.toml"))
     expected.add(
         ExpectedPath("wrapper manifest", _archive_path(WRAPPER_ARCHIVE_DIR, "pyproject.toml"), "pyproject.toml")
     )
