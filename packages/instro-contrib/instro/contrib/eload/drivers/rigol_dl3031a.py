598cfd55b0dfb6e08f8156a6c45cda40aa529659
nataliekozlowskii <nataliekozlowskii@gmail.com>
2026-07-29 23:27:07 -0400

    feat(contrib/eload): add initial Rigol DL3031A driver

---
+++ packages/instro-contrib/instro/contrib/eload/drivers/rigol_dl3031a.py
@@ -0,0 +1,74 @@
+"""
+RIGOL DL3031A DC eload driver
+"""
+
+from instro.eload import ELoadDriverBase
+from instro.eload.types import LoadMode, SlewRateDirection
+from instro.lib.transports.visa import VisaConfig, VisaDriver
+
+def loadmode_to_rigol(mode: LoadMode) -> str:
+    return {
+        LoadMode.CC: "CURRent",
+        LoadMode.CV: "VOLTage",
+        LoadMode.CP: "POWer",
+        LoadMode.CR: "RESistance",
+    }[mode]
+
+def slew_direction_to_rigol(direction: SlewRateDirection) -> str:
+    return {
+        SlewRateDirection.RISE: "POSitive",
+        SlewRateDirection.FALL: "NEGative",
+        SlewRateDirection.BOTH: "BOTH",
+    }[direction]
+
+class RigolDL3031A(ELoadDriverBase):
+    """
+    TODO: hardware validate base functionality
+    """
+
+    def __init__(self, visa_resource: str | VisaConfig) -> None:
+        self._visa = VisaDriver(visa_resource)
+        # track short status since not tracked in questionable status register
+        self._short_enabled = False # TODO: verify default state
+
+    def open(self) -> None:
+        self._visa.open()
+        # no explicit remote enable command
+        # TODO: verify auto-enter remote mode upon receiving any SCPI command
+
+    def close(self) -> None:
+        self._visa.close()
+
+    def short_output(self, enable: bool, channel: int) -> None:
+        # TODO: verify hold vs. toggle functionality of short button
+        if enable != self._short_enabled:
+            self._write_checked("SYSTem:KEY 33")
+            self._short_enabled = enable
+
+    def set_mode(self, mode: LoadMode, channel: int) -> None:
+        self._write_checked(f"FUNC {loadmode_to_rigol(mode)}")
+
+    def set_level(self, mode: LoadMode, value: float, channel: int, curr_limit: float | None) -> None:
+        self._write_checked(f"{loadmode_to_rigol(mode)}:LEVel:IMMediate {value}")
+        if mode is LoadMode.CV and curr_limit is not None:
+            self._write_checked(f"VOLTage:ILIMt {curr_limit}")
+
+    def set_range(self, mode: LoadMode, value: float, channel: int) -> None:
+        if mode == LoadMode.CP:
+            raise NotImplementedError(
+                "Rigol DL3031A has no :RANGe command in CP mode"
+            )
+        else:
+            self._write_checked(f"{loadmode_to_rigol(mode)}:RANGe {value}")
+
+    def set_slewrate(self, direction: SlewRateDirection, rate: float, channel: int) -> None:
+        self._write_checked(f"CURRent:SLEW:{slew_direction_to_rigol(direction)} {rate}")
+
+    def output_enable(self, enable: bool, channel: int) -> None:
+        self._write_checked(f"INPut {int(enable)}")
+
+    def get_current(self, channel: int) -> float:
+        return self._query_checked_float("MEASure:CURRent?")
+
+    def get_voltage(self, channel: int) -> float:
+        return self._query_checked_float("MEASure:VOLTage?")
