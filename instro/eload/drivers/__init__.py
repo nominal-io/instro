74d907206a6348e2533f9a9808f5870a092654b3
William Harwood <william.harwood@nominal.io>
2026-08-07 12:03:27 -0700

    feat(psu,eload): add support for EA PSB 10000 series

--- instro/eload/drivers/__init__.py
+++ instro/eload/drivers/__init__.py
@@ -3,4 +3,7 @@
 from instro.eload import ELoadDriverBase
 from instro.eload.drivers.bk_85xxb import BK85XXB
 
-__all__ = ["ELoadDriverBase", "BK85XXB"]
+# The PSB is a bidirectional power supply, so both its drivers live under the psu category.
+from instro.psu.drivers.ea_psb10000 import EAPSB10000VisaSink
+
+__all__ = ["ELoadDriverBase", "BK85XXB", "EAPSB10000VisaSink"]
