6b4db67400e5faa17bc3fd75dbc6a9ee8c56b368
John Hoehner <john.hoehner@nominal.io>
2026-08-11 15:56:38 -0700

    feat(unstable): add motor enable/disable to motor-controller contract

--- tests/unstable/motorcontroller/test_motorcontroller.py
+++ tests/unstable/motorcontroller/test_motorcontroller.py
@@ -27,6 +27,12 @@ class _StubDriver(MotorControllerDriverBase):
     def stop(self) -> None:
         self.calls.append(("stop",))
 
+    def enable(self) -> None:
+        self.calls.append(("enable",))
+
+    def disable(self) -> None:
+        self.calls.append(("disable",))
+
     def get_telemetry(self) -> MotorTelemetry:
         return self.telemetry
 
@@ -64,6 +70,8 @@ def test_open_close_delegate_to_driver(motor: InstroMotorController, driver: _St
     ("call", "channel", "value", "driver_call"),
     [
         (lambda m: m.stop_motor(), "m.stop.cmd", 1.0, ("stop",)),
+        (lambda m: m.motor_enable(), "m.enable.cmd", 1.0, ("enable",)),
+        (lambda m: m.motor_disable(), "m.enable.cmd", 0.0, ("disable",)),
         (lambda m: m.set_current(2.5), "m.current.cmd", 2.5, ("set_current", 2.5)),
     ],
 )
